a13dd33104509859cb1c37f7c3ae9d874d6a6802
Antoh1 <antotiro@gmail.com>
2022-10-07 16:38:58 +0300

    [ADD] Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+
+{
+    'name': 'Hospital Management',
+    'version': '11.0.1.0.0',
+    'summary': 'Module for managing the Hospital',
+    'description': '',
+    'category': 'Extra Tools',
+    'author': 'Tirop',
+    'maintainer': 'Tirop',
+    'website': 'tirop.com',
+    'license': 'AGPL-3',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'external_dependencies': {
+        'python': [],
+    }
+}
