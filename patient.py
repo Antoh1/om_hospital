9f2388e662e8f43e80b5e75ebbec11f045a4654a
Antoh1 <antotiro@gmail.com>
2022-10-13 15:31:54 +0300

    [UPDT] added age_group, a computed field with its fn

--- patient.py
+++ patient.py
@@ -16,12 +16,23 @@ class HospitalPatient(models.Model):
     _description = 'Patient Record'
 
     patient_name = fields.Char(string='Name', required=True)
-    patient_age = fields.Integer("Age")
+    patient_age = fields.Integer(string="Age", default=0)
     gender = fields.Selection([('fe_male', 'Female'), ('male', 'Male')], default="male", string="Gender")
     notes = fields.Text(string="Notes")
     image = fields.Binary(string="Image")
+    age_group = fields.Selection(string="Age Group", selection=[('minor', 'Minor'), ('adult', 'Adult')],
+                                 compute='set_age_group')
     name_seq = name = fields.Char(string='Patient Reference', required=True, copy=False, readonly=True,
                                   index=True, default=lambda self: _('New'))
+
+    @api.depends('patient_age')
+    def set_age_group(self):
+        for rec in self:
+            if rec.patient_age:
+                if rec.patient_age < 18:
+                    rec.age_group = 'minor'
+                else:
+                    rec.age_group = 'adult'
 
     @api.model
     def create(self, vals):
